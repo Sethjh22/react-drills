7a8384b2f434d60dbb0a7a260580798d64743bf5
Seth Hill <sethjoshuahill@gmail.com>
2021-03-03 15:47:22 -0700

    finished with app 2 best time 3:13

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      list: ['apples','Bananans', 'idk', 'Halo', 'Im happy']
+    }
+  }
+
+
+  render(){
+    let listDisplay = this.state.list.map((e, i)=> {
+      return <h2 key={i}>{e}</h2>
+    })
+    return(
+      <section className='App'>{listDisplay}</section>
+    )
+  }
 }
-
-export default App;
+export default App
