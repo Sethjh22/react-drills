9edc109953d987d8f89b3ceab1d77505468331d6
Seth Hill <sethjoshuahill@gmail.com>
2021-03-04 10:11:32 -0700

    finished  app 5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react'
+import './App.css'
+import Image from './Image'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+
+
+  render(){
+    return(
+      <div className='App'>
+        <Image url={'https://c4.wallpaperflare.com/wallpaper/757/525/281/rick-and-morty-tv-mozilla-firefox-rick-sanchez-wallpaper-preview.jpg'}/>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
